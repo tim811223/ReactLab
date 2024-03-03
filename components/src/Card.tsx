0016a0b8f42663397009412d8f3a44ee9db7527c
tim811223 <timforpg20190311@hotmail.com>
2024-03-03 14:43:39 +0800

    把css獨立到css檔

--- components/src/Card.tsx
+++ components/src/Card.tsx
@@ -1,6 +1,7 @@
 /* 這邊產生共用Card 的component */
 
 import {FC} from 'react';
+import './Card.css';
 
 export interface Props{
   image:string;
@@ -9,13 +10,10 @@ export interface Props{
 };
 
 export const Card :FC<Props> = ({image,name,description}) => {
-
-  const Style = {border:'1px solid red'};
-
   return(
     <div className='card'>
       <div className='card-image'>
-        <img src={image}  style={Style}></img>
+        <img src={image}></img>
       </div>
       <div className='card-content'>
         <h3 className='title is-3'>{name}</h3>
