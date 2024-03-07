791216905d77ca65ce0cab2676c9c30237938689
tim811223 <timforpg20190311@hotmail.com>
2024-03-07 23:24:37 +0800

    從Bluma加入Button模板

--- CH8-ReactEvent-thumps-up/thumps-up/src/ThumpsUp.tsx
+++ CH8-ReactEvent-thumps-up/thumps-up/src/ThumpsUp.tsx
@@ -1,7 +1,12 @@
+
 export const ThumpsUp = ()=>{
   let likes = 0;
   return (
-    <>
-    </>
+    <button className="button">
+    <span className="icon">
+      <i className="fab fa-github"></i>
+    </span>
+    <span>GitHub</span>
+  </button>
   );
 }
