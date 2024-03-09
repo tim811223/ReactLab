8a913d7809116edbef56d03a104095b873e84d57
tim811223 <timforpg20190311@hotmail.com>
2024-03-09 14:24:45 +0800

    (function Component方法)設定一個likes的變數,並且設定setLikes可以變更此變數,並且設定預設為0

--- CH8-ReactEvent-thumps-up/thumps-up/src/ThumpsUp.tsx
+++ CH8-ReactEvent-thumps-up/thumps-up/src/ThumpsUp.tsx
@@ -1,10 +1,11 @@
 import { faThumbsUp } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 
 export const ThumpsUp = ()=>{
-  let likes = 0;
+  const [likes,setLikes] = useState<number>(0);  //(function Component方法)設定一個likes的變數,並且設定setLikes可以變更此變數,並且設定預設為0
   return (
-    <button className="button is-info" onClick={()=> likes++}>
+    <button className="button is-info" onClick={()=> setLikes(likes+1)}>
     <span className="icon">
       <FontAwesomeIcon icon={faThumbsUp} />
     </span>
