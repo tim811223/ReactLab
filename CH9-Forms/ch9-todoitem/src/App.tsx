70137cd20821628b7eaa67eaa068da3e3c4e88c1
tim811223 <timforpg20190311@hotmail.com>
2024-03-11 16:40:43 +0800

    新增檔案 [src\ToDos\Editor.tsx , src\ToDos\ToDoItem.tsx , src\ToDos\ToDoList.tsx] ,並在Editor.tsx使用bilma css framework來完成表單畫面

--- CH9-Forms/ch9-todoitem/src/App.tsx
+++ CH9-Forms/ch9-todoitem/src/App.tsx
@@ -1,8 +1,11 @@
+import 'bulma/css/bulma-rtl.min.css';
 import { useState } from 'react'
+import { Editor } from './ToDos/Editor'
 
 function App() {
   return (
     <>
+      <Editor/>
     </>
   )
 }
